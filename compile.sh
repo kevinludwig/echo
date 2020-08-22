10a53e48abb43769b47e429d4575e117fed5de10
Kevin Ludwig <kevin.ludwig@gmail.com>
2020-08-22 15:20:51 -0700

    fix typo

--- compile.sh
+++ compile.sh
@@ -2,7 +2,7 @@
 
 gcc -o echoclient echoclient.c
 gcc -o echoserver_single_threaded echoserver_single_threaded.c
-gcc -o echoserver_multi_tbreaded -lpthread echoserver_multi_threaded.c
+gcc -o echoserver_multi_threaded -lpthread echoserver_multi_threaded.c
 gcc -o echoserver_select echoserver_select.c
 gcc -o echoserver_uv -luv echoserver_uv.c
 
