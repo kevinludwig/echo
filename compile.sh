0871007d620e0cf37e33e1d6aadf669cda739d3d
Kevin Ludwig <kevin.ludwig@gmail.com>
2020-08-22 15:19:57 -0700

    get the uv example working

--- compile.sh
+++ compile.sh
@@ -4,6 +4,6 @@ gcc -o echoclient echoclient.c
 gcc -o echoserver_single_threaded echoserver_single_threaded.c
 gcc -o echoserver_multi_tbreaded -lpthread echoserver_multi_threaded.c
 gcc -o echoserver_select echoserver_select.c
-# gcc -o echoserver_uv -luv echoserver_uv.c
+gcc -o echoserver_uv -luv echoserver_uv.c
 
 
--- echoserver_uv.c
+++ echoserver_uv.c
@@ -2,33 +2,34 @@
 #include <stdlib.h>
 #include <uv.h>
 
-uv_loop_t loop;
+uv_loop_t* loop;
 
-uv_buf_t on_alloc(uv_handle_t* handle, size_t size) {
-    return uv_buf_init((char *) malloc(size), size);
+void on_alloc(uv_handle_t* handle, size_t size, uv_buf_t* buf) {
+    buf->base = malloc(size);
+    buf->len = size;
 }
 
-void on_read(uv_stream_t* stream, ssize_t nread, uv_buf_t buf) {
-    uv_write_t* req = (uv_write_t *) malloc(sizeof(uv_write_t));
-
+void on_read(uv_stream_t* stream, ssize_t nread, const uv_buf_t* buf) {
     if (nread < 0) {
-        if (uv_last_error(loop).code != UV_EOF) {
-            fprintf(stderr, "error reading stream %s\n", uv_strerror(uv_last_error(loop)));
-        } 
         uv_close((uv_handle_t*) stream, NULL);
-    }
+    } else {
+        uv_write_t* req = (uv_write_t*) malloc(sizeof(uv_write_t));
 
-    int r = uv_write(req, stream, &buf, 1, NULL);
-    if (r) {
-        fprintf(stderr, "error on write %s\n", uv_strerror(uv_last_error(loop)));
-    }
+        printf("received %zd bytes, buf=%s", nread, buf->base);
 
-    free(buf.base);
+        int r = uv_write(req, stream, buf, 1, NULL);
+        if (r) {
+            fprintf(stderr, "error on write %s\n", uv_strerror(r));
+        }
+
+        free(req);
+        free(buf->base);
+    }
 }
 
 void on_connection(uv_stream_t* server, int status) {
     if (status < 0) {
-        fprintf(stderr, "new connection error %s\n", uv_strerror(uv_last_error(loop)));
+        fprintf(stderr, "new connection error %s\n", uv_strerror(status));
         return;
     }
 
@@ -37,7 +38,7 @@ void on_connection(uv_stream_t* server, int status) {
     if (uv_accept(server, (uv_stream_t*) client) == 0) {
         int r = uv_read_start((uv_stream_t*) client, on_alloc, on_read);
         if (r) {
-            fprintf(stderr, "error reading %s", uv_strerror(uv_last_error(loop)));
+            fprintf(stderr, "error reading %s", uv_strerror(r));
         }
     } else {
         uv_close((uv_handle_t*) client, NULL);
@@ -46,7 +47,7 @@ void on_connection(uv_stream_t* server, int status) {
 
 int main(int argc, char* argv[]) {
     if (argc != 2) {
-        fprintf("usage %s <port>\n", argv[0]);
+        fprintf(stderr, "usage %s <port>\n", argv[0]);
         exit(1);
     }
 
@@ -57,13 +58,18 @@ int main(int argc, char* argv[]) {
     uv_tcp_t server;
     uv_tcp_init(loop, &server);
 
-    uv_ip4_addr("0.0.0.0", port, &addr);
+    struct sockaddr_in addr;
 
+    uv_ip4_addr("0.0.0.0", port, &addr);
     uv_tcp_bind(&server, (const struct sockaddr*)&addr, 0);
-    int r = uv_listen((uv_stream_t*) &server, 10, on_new_connection);
+
+    int r = uv_listen((uv_stream_t*) &server, 10, on_connection);
     if (r) {
-        fprintf(stderr, "listen error %s\n", uv_strerror(uv_last_error(loop)));
+        fprintf(stderr, "listen error %s\n", uv_strerror(r));
         return 1;
     }
+
+    printf("listening on %d\n", port);
+    
     return uv_run(loop, UV_RUN_DEFAULT);
 }
